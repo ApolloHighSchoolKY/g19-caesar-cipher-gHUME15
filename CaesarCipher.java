b99ac51c57d7d85b58cfdc7398c30c35f9c5b7a2
gHUME15 <Grayson.hume@stu.daviess.kyschools.us>
2025-02-21 03:47:32 +0000

    I will have to finsh this tomorrow

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -9,29 +9,46 @@
 
 public class CaesarCipher {
 
-	private char[][] alphabet;
-	private char[][] shifted;
+	private char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+	private char[] shifted;
 	private int shift;
-
+//look through all the letters then use
+//Use a loop that goes through every letter
+// Use two diferent string with difernent names
+//If what your looking for is not found use bollen to add it back on dont change it
+//22+4% somthing
     public CaesarCipher() {
-
+        
     }
 
     public CaesarCipher(int num){
-
+        this.shift = num % 26; 
     }
+        public String encrypt(String message) {
+      StringBuilder encryptedMessage = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+         char currentChar = message.charAt(i);
+          if (Character.isLetter(currentChar)) {
+          char base = (Character.isLowerCase(currentChar))? 'a' : 'A'; // Not sure why i have to have a ? but it says missing either ) or ? and ? works so
+          int charIndex = (currentChar - base) % 26;
+           int newCharIndex = (charIndex + shift) % 26;
 
-    public String encrypt(String message){
-
+          char newChar = alphabet[newCharIndex];
+          if (Character.isUpperCase(currentChar)) {
+              newChar = Character.toUpperCase(newChar);
+     }
+     encryptedMessage.append(newChar);
+      } else {
+    encryptedMessage.append(currentChar);
+         }
     }
+    
+   return encryptedMessage.toString();
+  }
+  public String decrypt(String message){
+   return "";
+}
 
-    public String decrypt(String message){
-
-    }
-
-    public void shifter(int num){
-
-    }
-
-
+  public void shifter(int num){
+}
 }
--- G19.java
+++ G19.java
@@ -6,7 +6,7 @@
  * @version 1.00 2025/2/20
  */
 
-
+	//Use modulus
 public class G19 {
 
     public static void main(String[] args) {
@@ -19,7 +19,9 @@ public class G19 {
     	//Create a new cipher using the shifter(#) method
     	//Encrypt and decrypt a message with your new cipher
 
+		CaesarCipher Cipher = new CaesarCipher(4);
+        System.out.println("Encrypted: " + Cipher.encrypt("wow"));
+        System.out.println("Decrypted: " + Cipher.decrypt("asa"));
+
     }
-
-
 }
